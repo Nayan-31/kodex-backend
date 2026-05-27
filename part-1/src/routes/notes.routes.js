062f91e52a71b58f423b202562720d9f425dd961
Nayan Mahato <nayanmahato904@gmail.com>
2026-05-27 10:38:48 +0530

    routes comments added

--- part-1/src/routes/notes.routes.js
+++ part-1/src/routes/notes.routes.js
@@ -2,6 +2,27 @@ let express = require('express')
 
 let router = express.Router()
 
+/**
+* @route POST /create
+* @description create a new note need title and description in the request body
+* @access public
+*/
+
 router.post('/create')
+
+/**
+ * @route GET /get
+ * @description get all notes
+ * @access public
+ */
+
+router.get('/get')
+
+/**
+ * @route /:id
+ * @description update notes by id requires description for update 
+ * @access public
+ */
+
 
 module.exports = router
