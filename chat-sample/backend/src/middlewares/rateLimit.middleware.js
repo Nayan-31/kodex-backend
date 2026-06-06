40da683a82d2eb9b4095c30c42e6f41819d95a7d
Nayan Mahato <nayanmahato904@gmail.com>
2026-06-06 11:27:49 +0530

    rate limiter to 1000 req while you are in development mod e so you don't get blocked while building

--- chat-sample/backend/src/middlewares/rateLimit.middleware.js
+++ chat-sample/backend/src/middlewares/rateLimit.middleware.js
@@ -2,10 +2,13 @@ import rateLimit from 'express-rate-limit'
 
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100, // Limit each IP to 100 requests per windowMs
-  message: 'Too many requests, please try again later',
-  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
-  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+  max: process.env.NODE_ENV === 'development' ? 1000 : 100, // Increase limit for development
+  message: {
+      success: false,
+      message: 'Too many requests from this IP, please try again later'
+  },
+  standardHeaders: true,
+  legacyHeaders: false,
 })
 
 export default limiter
